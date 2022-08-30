8705726f137b915ac4b665fc207d1e0489793509
Nazanin FereydooniZade <na.fereydooni@yahoo.com>
2022-08-30 16:58:17 +0000

    generate preprocess file

--- lib/preprocessing/pheme/expand_contactions/expand_contractions_impl.py
+++ lib/preprocessing/pheme/expand_contactions/expand_contractions_impl.py
@@ -7,7 +7,7 @@ from lib.preprocessing.pheme.expand_contactions.expand_contractions import Expan
 class ExpandContractionsImpl(ExpandContractions):
     def expand(self, text):
         expanded_words = []
-        if text is numpy.NaN:
+        if text is numpy.NaN or text is None:
             return None
         for word in text.split():
             try:
